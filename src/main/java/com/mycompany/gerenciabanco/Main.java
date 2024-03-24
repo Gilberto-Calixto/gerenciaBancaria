fc3a471ac8d67bbc36c2efb592fd4d8789d613fb
AloneBoyy <gilbertocalixto2002@gmail.com>
2024-03-24 12:45:08 -0300

    Operações e menu prontos

--- src/main/java/com/mycompany/gerenciabanco/Main.java
+++ src/main/java/com/mycompany/gerenciabanco/Main.java
@@ -5,35 +5,62 @@ import java.util.Scanner;
 public class Main {
 
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         //bb testes
 
 
         //loop  para manter o usuário no programa até ele apertar em sair
         System.out.println("Bem-Vindo ao Banco javasil");
-        String encerrar;
+        int encerrar;
+
+        //pedirDados
+        DadosUsuario dadosUser = new DadosUsuario();
+        //dadosUser.pedirDados();
+        dadosUser.menu();
 
         do {
-            DadosUsuario dadosUser = new DadosUsuario();
-            dadosUser.pedirDados();
-
-            OperacoesBancarias operacoesBancarias = new OperacoesBancarias();
 
 
-            System.out.println("Deseja encerrar: ");
-            encerrar = dadosUser.scanner.next().toLowerCase();
 
-        }while (encerrar != "sim");
+
+            encerrar = dadosUser.scanner.nextInt();
+
+        }while (encerrar != 0);
     }
 
     //classe para receber os dados
     public static class DadosUsuario {
         public Scanner scanner = new Scanner(System.in);
+        OperacoesBancarias deposito = new OperacoesBancarias();
 
         private void menu(){
 
+            System.out.println("O que deseja: ");
+            System.out.println("""
+                    [1] Depositar
+                    [2] Sacar
+                    [3] Saldo
+                    [0] Encerrar
+                              \s
+                    """
+            );
+            int opcao = scanner.nextInt();
+            switch (opcao){
 
+                case 1:
+                    deposito.deposito();
+                    System.out.println("Deposito realizado com Sucesso.");
+                    break;
+                case 2:
+                    deposito.retirada();
+                    System.out.println("Operação realizada com Sucesso.");
+                    break;
+                case 3:
+                    System.out.println("Saldo: R$" + deposito.consultaSaldo());
+                    break;
+            }
+            menu();
 
         }
 
@@ -50,22 +77,41 @@ public class Main {
 
             System.out.println("CPF: ");
             int cpf = scanner.nextInt();
+
+
         }
+
+
     }
 
-    public class OperacoesBancarias{
+    public static class OperacoesBancarias{
 
-        double saldo;
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
 
-        private void consultaSaldo(){
-
+        private double consultaSaldo(){
+            return saldo;
         }
 
-        private void deposito(){
+        private double deposito(){
+            System.out.println("valor: R$");
+            double valor = scanner.nextDouble();
+            saldo += valor;
 
+            return saldo;
         }
 
-        private void retirada(){
+        private double retirada(){
+            System.out.println("valor: R$");
+            double retirada = scanner.nextDouble();
+            if (retirada >= saldo){
+                System.out.println("""
+                        Saldo insufuciênte\s
+                        Você tem disponivel: R$\s
+                        """ + saldo
+                );
+            }
+            return saldo -= retirada;
 
         }
     }
